9f399f084d62827f43cb56432746ae52a00f4ac4
Divya <divyabfalcon@gmail.com>
2026-06-01 21:32:50 -0700

    Fix CSP to allow Next.js inline scripts in production

--- prototypes/divya/next.config.js
+++ prototypes/divya/next.config.js
@@ -20,7 +20,7 @@ const csp = isDev
     ].join("; ")
   : [
       "default-src 'self'",
-      "script-src 'self'",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
       "style-src 'self' 'unsafe-inline'",
       "img-src 'self' data: https:",
       "font-src 'self' data:",
